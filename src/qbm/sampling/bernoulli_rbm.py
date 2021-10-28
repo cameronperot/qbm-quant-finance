7736b934553d6ab08cc1fbc6ea7a5455ed960669
Cameron Perot <c.perot@fz-juelich.de>
2021-10-28 18:35:36 +0200

    Updated Bernoulli RBM sampling

--- src/qbm/sampling/bernoulli_rbm.py
+++ src/qbm/sampling/bernoulli_rbm.py
@@ -19,31 +19,50 @@ def generate_sample(model, v, n_passes):
     for i in range(n_passes):
         v = model.gibbs(v)
 
-    return v.astype("int64")
+    return v
 
 
 def generate_samples_df(
-    model, n_visible, columns, n_samples, n_passes, split_points=[0, 16, 32, 48]
+    model,
+    n_samples,
+    n_visible,
+    n_passes,
+    columns,
+    split_points=[16, 32, 48],
+    sequential=True,
 ):
     """
     Generate a dataframe of samples.
 
     :param model: Scikitlearn RBM model.
-    :param n_visible: Number of visible units in the model.
-    :param columns: Names of the columns, must match the order of the binary strings.
     :param n_samples: Number of samples to generate.
-    :param v: Initial state to start the visible layer in.
+    :param n_visible: Number of visible units in the model.
     :param n_passes: Number of passes through the network to perform, i.e., the number of
+    :param columns: Names of the columns, must match the order of the binary strings.
+    :param v: Initial state to start the visible layer in.
         Gibbs sampling steps.
     :param split_points: Points at which to split the binary string.
+    :param sequential: Boolean to generate the samples sequentially (one starting random
+            input) if True, or in parallel (n_samples starting random inputs) if False.
 
     :returns: Dataframe of samples.
     """
     samples = []
-    for i in range(n_samples):
+
+    if sequential:
         v = np.random.choice([0, 1], n_visible)
-        v = generate_sample(model, v, n_passes)
-        v = convert_bin_list_to_str(v)
-        samples.append(split_bin_str(v, split_points))
+        for i in range(n_samples):
+            v = generate_sample(model, v, n_passes)
+            samples.append(
+                map(convert_bin_list_to_str, np.array_split(v, split_points))
+            )
+
+    else:
+        V = np.random.choice([0, 1], (n_samples, n_visible))
+        V = generate_sample(model, V, n_passes)
+        for v in V:
+            samples.append(
+                map(convert_bin_list_to_str, np.array_split(v, split_points))
+            )
 
     return pd.DataFrame(samples, columns=columns)
