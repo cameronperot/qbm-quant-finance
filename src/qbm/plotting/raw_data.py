5a7f9d202e59e6f0b5335e091776c08eb9623d3f
Cameron Perot <c.perot@fz-juelich.de>
2021-11-05 10:29:11 +0100

    Removed redundant argument in histogram plotting

--- src/qbm/plotting/raw_data.py
+++ src/qbm/plotting/raw_data.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 
-def plot_histogram_grid(df, currency_pairs, params, **kwargs):
+def plot_histogram_grid(df, params, **kwargs):
     """
     Plots a grid of historgrams for the desired feature.
 
@@ -12,8 +12,8 @@ def plot_histogram_grid(df, currency_pairs, params, **kwargs):
     :returns: Matplotlib figure and axes.
     """
     fig, axs = plt.subplots(2, 2, figsize=(10, 6), tight_layout=True, dpi=300)
-    for pair, ax in zip(currency_pairs, axs.flatten()):
-        ax.hist(df[pair], **kwargs)
+    for column, ax in zip(df.columns, axs.flatten()):
+        ax.hist(df[column], **kwargs)
         ax.set_title(pair)
         ax.set_xlim(params["xlims"])
         ax.set_ylim(params["ylims"])
