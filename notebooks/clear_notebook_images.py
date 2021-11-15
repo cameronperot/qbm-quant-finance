6b8e29004955df630eea783689498b74b85111ef
Cameron Perot <c.perot@fz-juelich.de>
2021-11-15 22:40:47 +0100

    Updated notebook

---
+++ notebooks/clear_notebook_images.py
@@ -0,0 +1,18 @@
+import json
+import os
+
+notebooks_dir = os.path.dirname(os.path.realpath(__file__))
+notebooks = [x for x in os.listdir(notebooks_dir) if x.endswith(".ipynb")]
+
+for notebook in notebooks:
+    with open(os.path.join(notebooks_dir, notebook), "r") as f:
+        nb = json.load(f)
+
+    for cell in nb["cells"]:
+        if cell.get("cell_type") == "code":
+            for output in cell.get("outputs", {}):
+                if "image/png" in output.get("data", {}):
+                    del output["data"]["image/png"]
+
+    with open(os.path.join(notebooks_dir, notebook), "w") as f:
+        json.dump(nb, f, indent=1)
