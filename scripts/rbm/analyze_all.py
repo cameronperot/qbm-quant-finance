8997366f4142ff35fd17e2a04f6202a8c4185a7c
Cameron Perot <c.perot@fz-juelich.de>
2021-11-18 12:16:14 +0100

    Fixed bug in analyze_all.py script args

--- scripts/rbm/analyze_all.py
+++ scripts/rbm/analyze_all.py
@@ -7,14 +7,14 @@ from analyze_ensemble import main as analyze_ensemble
 from analyze_autocorrelation import main as analyze_autocorrelation
 
 
-def main(analyze_ensemble, analyze_autocorrelation):
+def main(analyze_ensemble_bool, analyze_autocorrelation_bool):
     project_dir = get_project_dir()
     models = load_artifact(project_dir / "scripts/rbm/models.json")
 
     for model_name, model_info in models.items():
-        if analyze_ensemble:
+        if analyze_ensemble_bool:
             analyze_ensemble(model_info["id"])
-        if analyze_autocorrelation:
+        if analyze_autocorrelation_bool:
             analyze_autocorrelation(model_info["id"])
 
 
@@ -23,17 +23,19 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Analyze the model outputs.")
     parser.add_argument(
-        "--analyze_autocorrelation",
-        type=bool,
-        default=True,
+        "--no_analyze_autocorrelation",
+        default=False,
+        action="store_true",
         help="Do you want to analyze the autocorrelations?",
     )
     parser.add_argument(
-        "--analyze_ensemble",
-        type=bool,
-        default=True,
+        "--no_analyze_ensemble",
+        default=False,
+        action="store_true",
         help="Do you want to analyze the ensemble?",
     )
     args = parser.parse_args()
+    analyze_ensemble_bool = not args.no_analyze_ensemble
+    analyze_autocorrelation_bool = not args.no_analyze_autocorrelation
 
-    main(analyze_ensemble, analyze_autocorrelation)
+    main(analyze_ensemble_bool, analyze_autocorrelation_bool)
