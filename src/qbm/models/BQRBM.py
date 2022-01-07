c6a5980debd71f3a8c7162042e6fb4b8bf282b76
Cameron Perot <c.perot@fz-juelich.de>
2022-01-07 13:56:54 +0100

    Updated BQRBM WIP

--- src/qbm/models/BQRBM.py
+++ src/qbm/models/BQRBM.py
@@ -1,6 +1,5 @@
 import numpy as np
 from dwave.system import DWaveSampler, FixedEmbeddingComposite
-from minorminer import find_embedding
 from scipy.special import expit
 
 from qbm.models import QBMBase
@@ -19,22 +18,25 @@ class BQRBM(QBMBase):
         X,
         n_hidden,
         embedding,
-        annealing_params,
+        anneal_params,
+        anneal_schedule_data,
         β_initial=1.5,
         qpu_params={"region": "eu-central-1", "solver": "Advantage_system5.1"},
         seed=42,
+        exact=False,
     ):
         """
         :param X: Training data.
         :param n_hidden: Number of hidden units.
         :param embedding: Embedding of the problem onto the QPU.
-        :param annealing_params: Dictionary with the keys
-            ["schedule", "s_freeze", "A_freeze", "B_freeze", "relative_chain_strength"]
+        :param anneal_params: Dictionary with the keys
+            ["schedule", "s", "relative_chain_strength"]
             representing the annealing schedule (list of tuples), relative freeze time,
             A(s_freeze), B(s_freeze), and relative chain strength, respectively.
         :param β: Initial effective β of the D-Wave. Used for scaling the coefficients.
         :param qpu_params: Parameters dict to unpack for the qpu.
         :param seed: Seed for the random number generator (used for randomizing minibatches).
+        :param exact: If True will use an exact QBM computation rather than the annealer.
         """
         # convert from binary to ±1 if necessary
         if set(np.unique(X)) == set([0, 1]):
@@ -44,11 +46,35 @@ class BQRBM(QBMBase):
 
         super().__init__(X=X, n_hidden=n_hidden, seed=seed)
         self.embedding = embedding
-        self.annealing_params = annealing_params
-        self.β = β
-        self.βs = [β]
+        self.anneal_params = anneal_params
+        self.β = β_initial
+        self.βs = [β_initial]
+        self.A = anneal_schedule_data.loc[s, "A(s) (GHz)"]
+        self.B = anneal_schedule_data.loc[s, "B(s) (GHz)"]
+        self.exact = exact
 
         self._initialize_sampler()
+
+        if self.exact:
+            from qbm.utils.exact import (
+                compute_H,
+                compute_ρ,
+                sparse_X,
+                sparse_Z,
+                sparse_kron,
+            )
+
+            # set Kronecker product Pauli matrices
+            self.σ = {}
+            for i in range(self.n_qubits):
+                self.σ["x", i] = sparse_kron(i, self.n_qubits, sparse_X)
+                self.σ["z", i] = sparse_kron(i, self.n_qubits, sparse_Z)
+
+            # initialize states and state vectors
+            self.states = np.arange(2 ** self.n_qubits)
+            self.state_vectors = 1 - 2 * np.vstack(
+                [convert_bin_str_to_list(f"{x:{f'0{self.n_qubits}b'}}") for x in range(8)]
+            )
 
     def _initialize_sampler(self):
         """
@@ -57,64 +83,71 @@ class BQRBM(QBMBase):
         """
         self.qpu = DWaveSampler(**self.qpu_params)
         self.sampler = FixedEmbeddingComposite(self.qpu, self.embedding)
-        self.h_range = np.array(qpu.properties["h_range"])
-        self.J_range = np.array(qpu.properties["j_range"])
+        self.h_range = np.array(self.qpu.properties["h_range"])
+        self.J_range = np.array(self.qpu.properties["j_range"])
+
+    def _compute_mean_energy(self, V, H, VW):
+        """
+        Computes the mean classical energy w.r.t. the weights and biases over the provided
+        visible and hidden unit state vectors.
+
+        :param V: Numpy array where the rows are visible units.
+        :param H: Numpy array where the rows are hidden units.
+        :param VW: V @ W (used to avoid double computation).
+
+        :returns: Mean energy.
+        """
+        return (
+            -(V @ self.a).sum() - (H @ self.b).sum() - np.einsum("kj,kj", VW, H)
+        ) / V.shape[0]
 
     def _update_β(self):
         """
         Updates the effective β = 1 / kT. Used for scaling the coefficients sent to the
         annealer.
         """
-        # set the visible and hidden values to use for the training set calculations
+        n_samples = 10000
+
+        # sample state vectors
+        if self.exact:
+            state_vectors = self.sample_exact(n_samples)
+        else:
+            samples = self.sample(n_samples)
+            state_vectors = samples.record.sample
+
+        # compute the train energy
         V_train = self.X
         VW_train = V_train @ self.W
         H_train = expit(VW_train + self.b)
+        E_train = self._compute_energies(V_train, H_train, VW_train)
 
-        # compute the classical energy w.r.t. the training set
-        E_classical_train = (
-            -np.sum(V_train @ self.a) - np.sum(H_train @ self.b) - np.sum(VW_train, H_train)
-        ) / V.shape[0]
-
-        # TODO: compute the quantum energy w.r.t. the training set
-
-        # TODO: get samples
-
-        # set the visible and hidden values to use for the model calculations
-        V_model = samples.record.sample[:, : self.n_visible]
+        # compute the model energy
+        V_model = state_vectors[:, : self.n_visible]
         VW_model = V_model @ self.W
-        H_model = samples.record.sample[:, self.n_visible :]
+        H_model = state_vectors[:, self.n_visible :]
+        E_model = self._compute_energies(V_model, H_model, VW_model)
 
-        # TODO: compute the classical energy w.r.t. to the model
-        E_classical_model = (
-            -np.sum(V_model @ self.a) - np.sum(H_model @ self.b) - np.sum(VW_model, H_model)
-        ) / V.shape[0]
-
-        # TODO: compute the quantum energy w.r.t. to the model
-
-        # combine the energies
-        E_train = E_classical_train + E_quantum_train
-        E_model = E_classical_model + E_quantum_model
-
+        # update the params
         self.β += self.learning_rate / self.β ** 2 * (E_train - E_model)
         self.βs.append(self.β)
-        self.scaling_factor = self.β * self.annealing_params["B_freeze"]
+        self.scaling_factor = self.β * self.B
 
     def _compute_positive_grads(self, V_pos):
         """
         Computes the gradients for the positive phase, i.e., the expectation values w.r.t.
         the clamped Hamiltonian.
 
-        :param V: Numpy array where the rows are data vectors which to clamp the Hamiltonian
+        :param V_pos: Numpy array where the rows are data vectors which to clamp the Hamiltonian
             with.
         """
-        Γ = self.β * self.annealing_params["A_freeze"]
+        Γ = self.β * self.anneal_params["A"]
         b_eff = self.b + V_pos @ self.W
         D = np.sqrt(Γ ** 2 + b_eff ** 2)
         H_pos = (b_eff / D) * np.tanh(D)
 
         self.grads["a_pos"] = np.mean(V_pos, axis=0)
         self.grads["b_pos"] = np.mean(H_pos, axis=0)
-        self.grads["W_pos"] = V_pos.T @ H_pos / V.shape[0]
+        self.grads["W_pos"] = V_pos.T @ H_pos / V_pos.shape[0]
 
     def _compute_negative_grads(self, n_samples):
         """
@@ -123,9 +156,14 @@ class BQRBM(QBMBase):
 
         :param n_samples: Number of samples to obtain from the annealer.
         """
-        samples = self.sample(n_samples)
-        V_neg = samples.record.samples[:, :n_visible]
-        H_neg = samples.record.samples[:, n_visible:]
+        if self.exact:
+            state_vectors = self.sample_exact(n_samples)
+        else:
+            samples = self.sample(n_samples)
+            state_vectors = samples.record.sample
+
+        V_neg = state_vectors[:, : self.n_visible]
+        H_neg = state_vectors[:, self.n_visible :]
 
         self.grads["a_neg"] = V_neg.mean(axis=0)
         self.grads["b_neg"] = H_neg.mean(axis=0)
@@ -145,11 +183,27 @@ class BQRBM(QBMBase):
         self.b = np.clip(self.b, *bias_range)
         self.W = np.clip(self.W, *weight_range)
 
-    def sample(self, n_samples):
+    def sample(self, n_samples, answer_mode="raw", use_gauge=True):
+        """
+        Obtain a sample set using the annealer.
+
+        :param n_samples: Number of samples to generate (num_reads param in sample_ising).
+        :param answer_mode: "raw" or "histogram".
+        :param use_gauge: If True will use a random gauge transformation (recommended for
+            more robust sample generation).
+
+        :returns: Ocean SDK SampleSet object.
+        """
         # compute the h's and J's
         h = np.concatenate((self.a, self.b)) / self.scaling_factor
         J = np.zeros((self.n_qubits, self.n_qubits)) / self.scaling_factor
         J[: self.n_visible, self.n_visible :] = self.W / self.scaling_factor
+
+        # apply a random gauge
+        if use_gauge:
+            gauge = self.rng.choice([-1, 1], self.n_qubits)
+            h *= gauge
+            J *= np.outer(gauge, gauge)
 
         # compute the chain strength
         r = max(
@@ -169,10 +223,38 @@ class BQRBM(QBMBase):
             num_reads=n_samples,
             anneal_schedule=self.anneal_params["schedule"],
             chain_strength=chain_strength,
-            answer_mode="raw",
+            answer_mode=answer_mode,
+            auto_scale=False,
         )
 
+        # undo the gauge
+        if use_gauge:
+            samples.record.samples *= gauge
+
         return samples
+
+    def sample_exact(self, n_samples):
+        """
+        Sample using the exact computed probabilities.
+
+        :param n_samples: Number of samples to generate.
+
+        :returns: Samples array of shape (n_samples, n_qubits).
+        """
+        # compute the h's and J's
+        h = np.concatenate((self.a, self.b)) / self.scaling_factor
+        J = np.zeros((self.n_qubits, self.n_qubits)) / self.scaling_factor
+        J[: self.n_visible, self.n_visible :] = self.W / self.scaling_factor
+
+        # compute the Hamiltonian and density matrix
+        H = compute_H(h, J, self.A, self.B, self.n_qubits, self.σ)
+        ρ = compute_ρ(H, self.T)
+
+        # sample using the probabilities on the diagonal of ρ
+        sample_indices = self.rng.choice(self.states, size=n_samples, p=np.diag(ρ))
+        state_vectors = self.state_vectors[sample_indices]
+
+        return state_vectors
 
     def train(
         self,
